28a1b14d04008aa0d52a83b18baf79a15f8a97dd
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-06 21:38:12 -0500

    Create and seed airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Airport.create(Code: "ATL")
+Airport.create(Code: "DFW")
+Airport.create(Code: "DEN")
+Airport.create(Code: "ORD")
+Airport.create(Code: "LAX")
+Airport.create(Code: "JFK")
+Airport.create(Code: "LAS")
+Airport.create(Code: "MCO")
